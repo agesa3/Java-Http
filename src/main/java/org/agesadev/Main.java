f975389de290869247677842a0215fcc34b12978
agesa3 <collinsagesa2000@gmail.com>
2022-10-17 14:58:07 +0300

    cleaned the code

--- src/main/java/org/agesadev/Main.java
+++ src/main/java/org/agesadev/Main.java
@@ -4,6 +4,7 @@ package org.agesadev;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.agesadev.model.dto.Country;
 import org.agesadev.model.entity.CountryData;
 
@@ -12,11 +13,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
 
     private static final String BASE_URL = "https://api.covid19api.com/summary";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/covid";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "Lister7Lister@";
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
@@ -41,6 +50,27 @@ public class Main {
                         .date(country.getDate()).build();
                 System.out.println(countryData);
             });
+
+            try {
+                Class.forName(JDBC_DRIVER);
+                Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO country_covid_data (countryId, country_name, country_code, today_date) VALUES (?, ?, ?, ?)");
+                countryList.forEach(country -> {
+                    try {
+                        preparedStatement.setString(1, country.getID());
+                        preparedStatement.setString(2, country.getCountry());
+                        preparedStatement.setString(3, country.getCountryCode());
+                        preparedStatement.setString(4, country.getDate());
+                        preparedStatement.executeUpdate();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
